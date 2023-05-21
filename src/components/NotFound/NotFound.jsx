2c7b09943c54024ddcc73ba0bf2c30a15514796d
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-05-21 21:49:32 +0200

    Update NotFound component

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import notFoundImg from "../../img/not_foung_img.jpeg";
 import css from "./NotFound.module.css";
+import { useEffect } from "react";
 
 export const NotFound = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className={css.notFoundBox}>
       <p className={css.notFoundText}>This page wasn't found</p>
