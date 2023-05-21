30799d3cd724f1f4af182858e37bb79c83f489ef
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-05-21 21:24:08 +0200

    Updates

--- static/js/544.57d2c985.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktweets_cards=self.webpackChunktweets_cards||[]).push([[544],{9544:function(e,s,t){t.r(s);s.default=function(){}}}]);
-//# sourceMappingURL=544.57d2c985.chunk.js.map
